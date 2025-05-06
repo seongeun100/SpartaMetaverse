d0855a2acebfe7c6481548d5d7bfc7142b437671
Seongeun Baek <100seongeun@gmail.com>
2025-05-07 02:20:08 +0900

    NPC 대사추가, TalkUI 텍스트 추가
    - NPC 대사 추가
    - TalkUI 텍스트 추가

--- SpartaMetaverse_Project/Assets/Scripts/Metarverse/Manager/TalkManager.cs
+++ SpartaMetaverse_Project/Assets/Scripts/Metarverse/Manager/TalkManager.cs
@@ -17,11 +17,13 @@ public class TalkManager : MonoBehaviour
     {
         talkData.Add(1000, new string[] {
             "스파르타 메타버스에 온걸 환영합니다.",
-            "WASD, 화살표 키로 플레이어의 이동을 조작할 수 있고 SpaceBar를 누르면 점프를 할 수 있습니다.",
-            "위로 올라가면 보이는 문 앞에 가서 F키를 누르면 상호작용을 하실 수 있습니다.",
-            "그럼 즐거운 시간 보내세요."
+            "문 위쪽에 있는 안내자를 찾아가 보세요, 중요한 얘기를 해줄 거예요!"
             }); // 여러 대화를 위해 배열
-        talkData.Add(1001, new string[] { "안녕?", "난 게임NPC!" });
+        talkData.Add(1001, new string[] {
+            "반갑습니다, 플레이어님",
+            "위쪽으로 가시면 다양한 미니게임이 준비되어 있답니다.",
+            "그럼 즐거운 시간 보내세요."
+            });
         talkData.Add(1002, new string[] { "미구현입니다" });
         talkData.Add(1003, new string[] { "미구현입니다" });
     }
